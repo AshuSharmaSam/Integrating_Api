e64b173c5ec126a3426b704972097746222f38ee
AshuSharmaSam <ashusamsharma2484@gmail.com>
2021-05-19 10:09:02 +0530

    second

--- src/First.js
+++ src/First.js
@@ -5,16 +5,58 @@ import Table  from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 class First extends React.Component {
   
-  state ={
-    
-      items: []
+  state = {
+    name: '',
+    username:'',
+    email:'',
+    phone:'',
+    items:[],
+    id:''
   }
-  
+  handleChange1 = event => {
+    this.setState({ name: event.target.value });
+  }
+  handleChange2 = event => {
+    this.setState({ username: event.target.value });
+  }
+  handleChange3 = event => {
+    this.setState({ email: event.target.value });
+  }
+  handleChange4 = event => {
+    this.setState({ phone: event.target.value });
+  }
+  handleChange5 = event => {
+    this.setState({ id: event.target.value });
+  }
+ 
+ 
   componentDidMount() {
     axios.get(`https://jsonplaceholder.typicode.com/users`)
       .then(res => {
-        const items = res.data;
+        let  items = res.data;
         this.setState({ items });
+      })
+  }
+   handleSubmit = event => {
+    event.preventDefault();
+
+     let user= {
+       id:this.state.id,
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      username: this.state.username
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`,{user})
+      .then(res => {
+        console.log(res.data)
+        
+        this.setState(prevState => ({
+        items: [...prevState.items, user]
+      }))
+
+       
       })
   }
     deleteRow(id, e){  
@@ -26,9 +68,32 @@ class First extends React.Component {
       })  
     
   }  
-  
   render() {
       return (
+        <div>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+            Id:
+            <input type="text" name="id" onChange={this.handleChange5} />
+          </label>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange1} />
+          </label>
+          <label>
+            User Name:
+            <input type="text" name="username" onChange={this.handleChange2} />
+          </label>
+          <label>
+            Email:
+            <input type="text" name="email" onChange={this.handleChange3} />
+          </label>
+          <label>
+            Phone:
+            <input type="text" name="phone" onChange={this.handleChange4} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
         <Table striped bordered hover>
   
   <thead>
@@ -51,18 +116,20 @@ class First extends React.Component {
         <td>{item.phone}</td>
         <td><button className="btn btn-danger" onClick={(e) => this.deleteRow(item.id, e)}>Delete</button> </td>
         </tr>
+        
       )
     })}
+   
     
    
     
     
   </tbody>
 </Table>
-
+</div>
       );
     }
-  }
+}
 
 
 export default First;
--- src/Second.js
+++ src/Second.js
@@ -1,108 +1,59 @@
-import React from 'react';
+import {React,Component} from 'react';
 import axios from 'axios';
 import Table  from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
-export default class Second extends React.Component {
-  state = {
-    name: '',
-    username:'',
-    email:'',
-    phone:'',
-    items:[]
-  }
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(res => {
-        const items = res.data;
-        this.setState({ items });
-      })
-  }
-  handleChange1 = event => {
-    this.setState({ name: event.target.value });
-  }
-  handleChange2 = event => {
-    this.setState({ username: event.target.value });
-  }
-  handleChange3 = event => {
-    this.setState({ email: event.target.value });
-  }
-  handleChange4 = event => {
-    this.setState({ phone: event.target.value });
-  }
+class Second extends Component {
 
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const items = {
-      name: this.state.name,
-      email: this.state.email,
-      phone: this.state.phone,
-      username: this.state.username
-    };
-
-    axios.post(`https://jsonplaceholder.typicode.com/users`, { items })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
-  }
-
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person Name:
-            <input type="text" name="name" onChange={this.handleChange1} />
-          </label>
-          <label>
-            User Name:
-            <input type="text" name="username" onChange={this.handleChange2} />
-          </label>
-          <label>
-            Email:
-            <input type="text" name="email" onChange={this.handleChange3} />
-          </label>
-          <label>
-            Phone:
-            <input type="text" name="phone" onChange={this.handleChange4} />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-        <Table striped bordered hover>
-  
-  <thead>
-    <tr>
-      <th>id</th>
-      <th>Name</th>
-      <th>UserName</th>
-      <th>Email</th>
-      <th>Phone</th>
-    </tr>
-  </thead>
-  <tbody>
-    {this.state.items.map((item)=>{
-      return(
-        <tr>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
-        <td>{item.username}</td>
-        <td>{item.email}</td>
-        <td>{item.phone}</td>
-      </tr>
-      )
-    })}
-    
+   constructor(props) {
+      super(props) 
+      this.state = { //state is by default an object
+         students: [
+            { id: 1, name: 'John', age: 21, email: 'jf@email.com' },
+            { id: 2, name: 'Mak', age: 19, email: 'mak@email.com' },
+            { id: 3, name: 'Rock', age: 16, email: 'rock@email.com' },
+            { id: 4, name: 'Gaurav', age: 25, email: 'Gaurav@email.com' }
+         ]
+      }
+      this.addStudent = this.addStudent.bind(this);
+   }
    
-    
-    
-  </tbody>
-</Table>
+   addStudent () {
 
-        
-      </div>
-    )
-  }
+    const newStudent = { id: 5, name: 'Mayank', age: 31, email: 'mayankd@email.com' };
+
+    this.setState(prevState => ({
+        students: [...prevState.students, newStudent ]
+      }));
+     
+   }
+
+   renderTableData() {
+    return this.state.students.map((student, index) => {
+       const { id, name, age, email } = student //destructuring
+       return (
+          <tr key={id}>
+             <td>{id}</td>
+             <td>{name}</td>
+             <td>{age}</td>
+             <td>{email}</td>
+          </tr>
+       )
+    })
+ }
+
+   render() { 
+      return (
+         <div>
+            <h1>React Dynamic Table</h1>
+            <table id='students'>
+               <tbody>
+                  {this.renderTableData()}
+               </tbody>
+            </table>
+            <button onClick={this.addStudent}>Add Student</button>
+         </div>
+      )
+   }
 }
+
+export default Second;
